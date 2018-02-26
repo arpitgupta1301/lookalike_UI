8fc10a7d54d5cb1a283cb5bed720e3f8379cb988
Sandhya Ankush Shitole <Sandhya.Shitole@pb.com>
2018-02-26 11:43:53 +0530

    fix

--- app/modules/retention/controller/retention.controller.js
+++ app/modules/retention/controller/retention.controller.js
@@ -7,7 +7,7 @@
                                                  $uibModal,
                                                  $http,
                                                  $scope,
-                                                 $interval) {
+                                                 $interval, $timeout) {
       var _this = this;
 
       _this.colors = [
@@ -30,10 +30,9 @@
       ];
 
       _this.commonFilter = {};
-
       _this.monthToExpiry = {
         labels: ['3M', '6M', '12M', '18M'],
-        colors: ['#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE'],
+        colors: ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'],
         data: [
           []
         ],
@@ -48,25 +47,18 @@
               align: 'start',
               offset: 10
             }
+          },
+          scales: {
+            xAxes: [{
+              barPercentage: 0.4
+            }]
           }
-          /*
-                    tooltips: {
-                      callbacks: {
-                        labelTextColor: function (tooltipItem, chart) {
-                          return '#543453';
-                        },
-                        title: function (tooltipItem) {
-                          return 'Lease Expires in Months Distribution: ' + tooltipItem[0].xLabel + '\n' +
-                            'Count of Ship To Customer: ' + tooltipItem[0].yLabel;
-                        }
-                      }
-                    }
-          */
         }
       }
       _this.meterType = {
         labels: ['BOL', 'MOL', 'TOL'],
-        colors: ['#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE'],
+        colors: ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+          'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'],
         data: [[]],
         options: {
           plugins: {
@@ -84,7 +76,8 @@
       };
       _this.customeGroup = {
         labels: ['Commercial', 'Government', 'Strategic'],
-        colors: ['#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE'],
+        colors: ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+          'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'],
         data: [[]],
         options: {
           plugins: {
@@ -97,12 +90,18 @@
               align: 'start',
               offset: 10
             }
+          },
+          scales: {
+            xAxes: [{
+              barPercentage: 0.4
+            }]
           }
         }
       };
       _this.cancellation = {
         labels: ['0-20%', '21-40%', '41-60%', '61-80%', '81+%', 'Unscored'],
-        colors: ['#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE'],
+        colors: ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+          'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'],
         data: [[]],
         options: {
           plugins: {
@@ -115,13 +114,18 @@
               align: 'start',
               offset: 10
             }
+          },
+          scales: {
+            xAxes: [{
+              barPercentage: 0.4
+            }]
           }
         }
       };
-
       _this.activityStatus = {
         labels: ['Active', 'Sleep', 'Na'],
-        colors: ['#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE'],
+        colors: ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+          'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'],
         data: [[]],
         options: {
           plugins: {
@@ -134,12 +138,18 @@
               align: 'start',
               offset: 10
             }
+          },
+          scales: {
+            xAxes: [{
+              barPercentage: 0.4
+            }]
           }
         }
       }
       _this.digitalIndex = {
         labels: ['High', 'Medium', 'Low'],
-        colors: ['#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE', '#1F75FE'],
+        colors: ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+          'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'],
         data: [[]],
         options: {
           plugins: {
@@ -152,123 +162,229 @@
               align: 'start',
               offset: 10
             }
+          },
+          scales: {
+            xAxes: [{
+              barPercentage: 0.4
+            }]
           }
         }
       };
 
+
+      _this.selectedBarIndex = {
+        'lease_expiry': -1,
+        'type': -1,
+        'status': -1,
+        'probability': -1,
+        'd_category': -1,
+        'c_group': -1
+      };
+
       _this.resetFilters = function () {
         _this.commonFilter = {};
-        _this.getDataAsPerCurrentFilters();
-      }
-
-      _this.onClick = function (points, evt) {
-        var obj = {};
-        obj[points[0]._chart.canvas.id] = points[0]._model.label
-        angular.extend(_this.commonFilter, obj);
+        _this.selectedBarIndex = {
+          'lease_expiry': -1,
+          'type': -1,
+          'status': -1,
+          'probability': -1,
+          'd_category': -1,
+          'c_group': -1
+        };
         _this.getDataAsPerCurrentFilters();
       };
 
-      _this.getDataAsPerCurrentFilters = function () {
+      _this.onClick = function (points, elements) {
+        var obj = {};
+        obj[points[0]._chart.canvas.id] = points[0]._model.label
+        angular.extend(_this.commonFilter, obj);
+        _this.getDataAsPerCurrentFilters(points[0]._chart.canvas.id, points[0]['$datalabels']._index);
+      };
+
+      _this.getDataAsPerCurrentFilters = function (id, index) {
         var filter = {
           current: 'd_category',
           commonFilter: _this.commonFilter
         };
-
-        $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
-          angular.extend({}, filter, {current: 'lease_expiry'})).then(
-          function (response) {
-            if (response.data) {
-              var labels = [];
-              var values = [];
-              for (var key in response.data) {
-                labels.push(key);
-                values.push(response.data[key]);
+        if (id !== 'lease_expiry') {
+          $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
+            angular.extend({}, filter, {current: 'lease_expiry'})).then(
+            function (response) {
+              if (response.data) {
+                var labels = [];
+                var values = [];
+                for (var key in response.data) {
+                  labels.push(key);
+                  values.push(response.data[key]);
+                }
+                _this.monthToExpiry.data = values;
+                _this.monthToExpiry.labels = labels;
+                if (_this.selectedBarIndex[id] != -1) {
+                  _this.customeGroup.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+                    'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+                  _this.customeGroup.colors[_this.selectedBarIndex[id]] = "rgba(31,117,254,1)";
+                }
               }
-              _this.monthToExpiry.data = values;
-              _this.monthToExpiry.labels = labels;
-            }
-          }, function (error) {
+            }, function (error) {
+            });
+        } else if (id === 'lease_expiry' && index > -1) {
+          $timeout(function () {
+            _this.selectedBarIndex[id] = index;
+            _this.monthToExpiry.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+              'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+            _this.monthToExpiry.colors[index] = "rgba(31,117,254,1)";
           });
+        }
 
-        $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
-          angular.extend({}, filter, {current: 'probability'})).then(
-          function (response) {
-            if (response.data) {
-              var labels = [];
-              var values = [];
-              for (var key in response.data) {
-                labels.push(key);
-                values.push(response.data[key]);
+        if (id !== 'probability') {
+          $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
+            angular.extend({}, filter, {current: 'probability'})).then(
+            function (response) {
+              if (response.data) {
+                var labels = [];
+                var values = [];
+                for (var key in response.data) {
+                  labels.push(key);
+                  values.push(response.data[key]);
+                }
+                _this.cancellation.data = values;
+                _this.cancellation.labels = labels;
+                if (_this.selectedBarIndex[id] != -1) {
+                  _this.customeGroup.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+                    'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+                  _this.customeGroup.colors[_this.selectedBarIndex[id]] = "rgba(31,117,254,1)";
+                }
               }
-              _this.cancellation.data = values;
-              _this.cancellation.labels = labels;
-            }
-          }, function (error) {
+            }, function (error) {
+            });
+        } else if (id === 'probability' && index > -1) {
+          $timeout(function () {
+            _this.selectedBarIndex[id] = index;
+            _this.cancellation.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+              'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+            _this.cancellation.colors[index] = "rgba(31,117,254,1)";
           });
+        }
 
-        $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
-          angular.extend({}, filter, {current: 'status'})).then(
-          function (response) {
-            if (response.data) {
-              var labels = [];
-              var values = [];
-              for (var key in response.data) {
-                labels.push(key);
-                values.push(response.data[key]);
+        if (id !== 'status') {
+          $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
+            angular.extend({}, filter, {current: 'status'})).then(
+            function (response) {
+              if (response.data) {
+                var labels = [];
+                var values = [];
+                for (var key in response.data) {
+                  labels.push(key);
+                  values.push(response.data[key]);
+                }
+                _this.activityStatus.data = values;
+                _this.activityStatus.labels = labels;
+                if (_this.selectedBarIndex[id] != -1) {
+                  _this.customeGroup.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+                    'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+                  _this.customeGroup.colors[_this.selectedBarIndex[id]] = "rgba(31,117,254,1)";
+                }
               }
-              _this.activityStatus.data = values;
-              _this.activityStatus.labels = labels;
-            }
-          }, function (error) {
+            }, function (error) {
+            });
+        } else if (id === 'status' && index > -1) {
+          $timeout(function () {
+            _this.selectedBarIndex[id] = index;
+            _this.activityStatus.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+              'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+            _this.activityStatus.colors[index] = "rgba(31,117,254,1)";
           });
+        }
 
-        $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
-          angular.extend({}, filter, {current: 'd_category'})).then(
-          function (response) {
-            if (response.data) {
-              var labels = [];
-              var values = [];
-              for (var key in response.data) {
-                labels.push(key);
-                values.push(response.data[key]);
+        if (id !== 'd_category') {
+          $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
+            angular.extend({}, filter, {current: 'd_category'})).then(
+            function (response) {
+              if (response.data) {
+                var labels = [];
+                var values = [];
+                for (var key in response.data) {
+                  labels.push(key);
+                  values.push(response.data[key]);
+                }
+                _this.digitalIndex.data = values;
+                _this.digitalIndex.labels = labels;
+                if (_this.selectedBarIndex[id] != -1) {
+                  _this.customeGroup.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+                    'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+                  _this.customeGroup.colors[_this.selectedBarIndex[id]] = "rgba(31,117,254,1)";
+                }
               }
-              _this.digitalIndex.data = values;
-              _this.digitalIndex.labels = labels;
-            }
-          }, function (error) {
+            }, function (error) {
+            });
+        } else if (id === 'd_category' && index > -1) {
+          $timeout(function () {
+            _this.selectedBarIndex[id] = index;
+            _this.digitalIndex.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+              'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+            _this.digitalIndex.colors[index] = "rgba(31,117,254,1)";
           });
+        }
 
-        $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
-          angular.extend({}, filter, {current: 'type'})).then(
-          function (response) {
-            if (response.data) {
-              var labels = [];
-              var values = [];
-              for (var key in response.data) {
-                labels.push(key);
-                values.push(response.data[key]);
+        if (id !== 'type') {
+          $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
+            angular.extend({}, filter, {current: 'type'})).then(
+            function (response) {
+              if (response.data) {
+                var labels = [];
+                var values = [];
+                for (var key in response.data) {
+                  labels.push(key);
+                  values.push(response.data[key]);
+                }
+                _this.meterType.data = values;
+                _this.meterType.labels = labels;
+                if (_this.selectedBarIndex[id] != -1) {
+                  _this.customeGroup.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+                    'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+                  _this.customeGroup.colors[_this.selectedBarIndex[id]] = "rgba(31,117,254,1)";
+                }
               }
-              _this.meterType.data = values;
-              _this.meterType.labels = labels;
-            }
-          }, function (error) {
+            }, function (error) {
+            });
+        } else if (id === 'type' && index > -1) {
+          $timeout(function () {
+            _this.selectedBarIndex[id] = index;
+            _this.meterType.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+              'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+            _this.meterType.colors[index] = "rgba(31,117,254,1)";
           });
+        }
 
-        $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
-          angular.extend({}, filter, {current: 'c_group'})).then(
-          function (response) {
-            if (response.data) {
-              var labels = [];
-              var values = [];
-              for (var key in response.data) {
-                labels.push(key);
-                values.push(response.data[key]);
+        if (id !== 'c_group') {
+          $http.post('https://lookalike-service-temp-dot-datatest-148118.appspot.com/activeLeases',
+            angular.extend({}, filter, {current: 'c_group'})).then(
+            function (response) {
+              if (response.data) {
+                var labels = [];
+                var values = [];
+                for (var key in response.data) {
+                  labels.push(key);
+                  values.push(response.data[key]);
+                }
+                _this.customeGroup.data = values;
+                _this.customeGroup.labels = labels;
+                if (_this.selectedBarIndex[id] != -1) {
+                  _this.customeGroup.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+                    'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+                  _this.customeGroup.colors[_this.selectedBarIndex[id]] = "rgba(31,117,254,1)";
+                }
               }
-              _this.customeGroup.data = values;
-              _this.customeGroup.labels = labels;
-            }
-          }, function (error) {
+            }, function (error) {
+            });
+        } else if (id === 'c_group' && index > -1) {
+          $timeout(function () {
+            _this.selectedBarIndex[id] = index;
+            _this.customeGroup.colors = ['rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)',
+              'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)', 'rgba(31,117,254,0.5)'];
+            _this.customeGroup.colors[index] = "rgba(31,117,254,1)";
           });
+        }
       }
       _this.getDataAsPerCurrentFilters();
     });
